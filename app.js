837aa3f02cf5905e1759bb5fc450728e86f91183
Marek Pelka <pelkam95@gmail.com>
2018-05-29 14:46:27 +0200

    Added environment variables

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ const multer = require('multer');
 
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
+dotenv.load({ path: '.env.example' });
+
 const passportConfig = require('./config/passport');
 
 /**
